d024161c45d75ecaaa59ac109fb99ef457384430
Ismail Abousalem <ia.abousalem@gmail.com>
2024-09-28 17:50:33 -0400

    Created AI and made chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import './styles/tailwind.css';
+import Chatbot from './components/Chatbot';
 import { FinancialHealthTrackerComponent } from '/src/components/financial-health-tracker';  // Adjust the path if necessary
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Welcome to Prospera</h1>
       <FinancialHealthTrackerComponent />
+      <Chatbot></Chatbot>
     </div>
   );
 }
